bc61f1f7041f72084f981997d995b7f42127c195
meindert.boterman <nlmbot2@gmail.com>
2020-05-24 09:31:42 +0200

    change 5

--- contacts/routes/index.js
+++ contacts/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { pgconn } = require('../db/config')
 
-/* Show home page. */
+/* Show home page. */ change 5
 router.get('/', function(req, res) {
   // we first check if the 'contacts' table exists
   pgconn.query("SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'contacts')", function(err,results) {
